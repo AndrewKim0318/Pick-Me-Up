6b58d9e58d21cd91ec0780a7138b10d54d8d9bc2
Amanda Shalansky <ashalansky20@hotmail.com>
2019-08-24 17:47:40 +0000

    updates server, schema files

---
+++ db/schema/02_food_items.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS food_items CASCADE;
+
+CREATE TABLE food_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  cost SMALLINT NOT NULL,
+  description TEXT
+  );
