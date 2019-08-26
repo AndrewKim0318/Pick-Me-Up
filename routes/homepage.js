22e766441faf220274b223ea3f5afc1c01be5d5b
Andrew Kim <wk@ualberta.ca>
2019-08-26 20:15:25 +0000

    working on changing menu item content based on what category is clicked

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router  = express.Router();
 const bcrypt  = require("bcrypt");
-const users   = require("../server");
+const { users, foodItems }  = require("./constants");
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
+    console.log("this", this);
     const id = req.session.userId;
     const templateVars = {
-      user: users[id]
+      user: users[id],
+      fooditems: foodItems
     }
     res.render('homepage', templateVars);
   });
