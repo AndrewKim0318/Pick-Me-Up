a5efcb6ff4c512ca05d051196f7ba30ca5ff6ca6
Andrew Kim <wk@ualberta.ca>
2019-08-26 18:21:27 +0000

    added onclick functions to food item nav bar

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,20 +1,15 @@
 $(() => {
   const $searchContainer = $(".search-container");
   const $searchIcon = $("#search-icon");
+  const $pastryButton = $("#pastries");
+  const $cookieButton = $("#cookies");
+  const $cakeButton = $("#cakes");
+  const $pieButton = $("#pies");
+  const $macaronButton = $("#macarons");
+  const $menuItems = $(".menu-item");
   $searchContainer.hide();
-  console.log($searchContainer[0]);
 
-
-  // $.ajax({
-  //   method: "GET",
-  //   url: "/api/users"
-  // }).done((users) => {
-  //   for(user of users) {
-  //     $("<div>").text(user.name).appendTo($("body"));
-  //   }
-  // });
-
-  $(".search-icon").click(function(event) {
+  $searchIcon.click(function(event) {
     event.preventDefault();
 
     $searchContainer.animate({width: "toggle"});
@@ -26,5 +21,35 @@ $(() => {
       $searchIcon.addClass("fa-search");
       $searchIcon.removeClass("fa-times");
     }
-  })
+  });
+
+  $pastryButton.click(function(event) {
+    event.preventDefault();
+
+    $menuItems.empty();
+  });
+
+  $cookieButton.click(function(event) {
+    event.preventDefault();
+
+    $menuItems.empty();
+  });
+
+  $cakeButton.click(function(event) {
+    event.preventDefault();
+
+    $menuItems.empty();
+  });
+  
+  $pieButton.click(function(event) {
+    event.preventDefault();
+
+    $menuItems.empty();
+  });
+  
+  $macaronButton.click(function(event) {
+    event.preventDefault();
+
+    $menuItems.empty();
+  });
 });
