df76678617aab7ce5ab7763dbc0810052cce37c4
Andrew Kim <wk@ualberta.ca>
2019-08-27 15:21:38 +0000

    changed all the double quotes into single quotes in seeds and changed homepage and login to use the database

--- routes/homepage.js
+++ routes/homepage.js
@@ -5,13 +5,30 @@ const { users, foodItems }  = require("./constants");
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    console.log("this", this);
     const id = req.session.userId;
-    const templateVars = {
-      user: users[id],
-      foodItems: foodItems
-    }
-    res.render('homepage', templateVars);
+    
+    queryString = `
+      SELECT *
+      FROM users
+      WHERE id = $1;
+    `;
+
+    queryParams = [id];
+
+    db.query(queryString, queryParams)
+    .then(res => {
+      if(res.rows.length > 0){
+        return res.rows;
+      }
+      return null;
+    })
+    .then(user => {
+      const templateVars = {
+        user: user,
+        foodItems: foodItems
+      }
+      res.render('homepage', templateVars); 
+    });
   });
   return router;
-};
+}
--- routes/login.js
+++ routes/login.js
@@ -14,27 +14,63 @@ const getIdByUsername = function(database, username) {
 module.exports = (db) => {
   router.get("/", (req, res) => {
     const id = req.session.userId;
-    const templateVars = {
-      user: users[id]
-    }
   
-    if (users[id]) {
-      res.redirect('/');
-    } else {
-      res.render('login', templateVars);
-    }
+    const queryString = `
+      SELECT *
+      FROM users
+      WHERE id = $1;
+    `;
+
+    const queryParams = [id];
+
+    db.query(queryString, queryParams)
+    .then(res => {
+      if(res.rows.length > 0){
+        return res.rows;
+      }
+      return null;
+    })
+    .then(user => {
+      if (user !== null) {
+        res.redirect('/');
+      } else {
+        const templateVars = {
+          user: user
+        }
+        res.render('login', templateVars);
+      }
+    });
+
   });
 
   router.post("/", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    const id = getIdByUsername(users, username);
+    const queryString = `
+      SELECT *
+      FROM users
+      WHERE username = $1;
+    `;
 
-    if (bcrypt.compareSync(password, users[id]["password"])) {
-      req.session.userId = id;
+    const queryParams = [username];
+
+    db.query(queryString, queryParams)
+    .then(res => res.rows[0])
+    .then(user => {
+      if (bcrypt.compareSync(password, user["password"])) {
+        return user;
+      }
+      return null;
+    })
+    .then(user => {
+      if(!user) {
+        res.send({error: "error"});
+        return;
+      }
+      req.session.userId = user.id;
       res.redirect("/");
-    } 
+    });
 
   });
   return router;
