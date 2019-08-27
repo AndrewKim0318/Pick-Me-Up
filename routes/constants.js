3726646b7f11a4874b8c2dd6a476819ffb90e09d
Amanda Shalansky <ashalansky20@hotmail.com>
2019-08-27 00:25:28 +0000

    fixed menu item container

--- routes/constants.js
+++ routes/constants.js
@@ -1,37 +1,49 @@
 const users = {
-  
+
 };
 
 const foodItems = {
   1: {
-    name: "scones",
+    name: "Scones",
     description: "3 freshly baked blueberry scones",
     price: "8.50",
     category: "pastries"
   },
   2: {
-    name: "cookies",
+    name: "Cookies",
     description: "3 freshly baked blueberry cookies",
     price: "2.50",
     category: "cookies"
   },
   3: {
-    name: "cake",
+    name: "Cake",
     description: "1 nice cake",
     price: "15.50",
     category: "cakes"
   },
   4: {
-    name: "pie",
-    description: "1 apple pie",
+    name: "Pie",
+    description: "1 apple pie that is super super super good",
     price: "12.50",
     category: "pies"
   },
   5: {
-    name: "macarons",
+    name: "Macarons",
     description: "3 mystery flavoured macarons",
     price: "7.50",
     category: "macarons"
+  },
+  6: {
+    name: "Black Forest",
+    description: "delicious black forest cake",
+    price: "14.50",
+    category: "cakes"
+  },
+  7: {
+    name: "Cheese Scone",
+    description: "cheese filled scone",
+    price: "5.00",
+    category: "pastries"
   }
 }
 
